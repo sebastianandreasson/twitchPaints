4007e850cd7c38d5d2070be90e2a631b6a1f81fb
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2017-04-01 14:32:49 +0200

    remove stuff we wont use

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 import socketIO from "socket.io";
 import async from "async";
-import moment from "moment";
 import _ from "underscore";
 import express from "express";
 import bodyParser from "body-parser";
@@ -8,7 +7,6 @@ import multer from "multer";
 import debug_module from "debug";
 
 import chatBot from "./chatbot";
-import painter from "./painter";
 
 const debug = debug_module("server");
 const debug_socket = debug_module("socket");
@@ -19,170 +17,74 @@ const io = socketIO.listen(1338);
 io.set("log level", 1);
 
 const requestQueue = async.queue((request, callback) => {
-    debug(request);
-    if (connectedClient){
-        connectedClient.emit(request.name, request.payload);
-    }
-    callback();
+  debug(request);
+  if (connectedClient){
+    connectedClient.emit(request.name, request.payload);
+  }
+  callback();
 });
 
-const sessionQueue = async.queue((request, callback) => {
-    if (connectedClient) {
-        debug("start session: " + request.name);
-        const votingWinner = chatBot.getVotingWinner() ? chatBot.getVotingWinner().name : null;
-        switch (request.name) {
-            case "endTheme":
-                if (votingWinner){
-                    request.payload.themeName = votingWinner;
-                }
-                break;
-            case "endNaming":
-                if (votingWinner){
-                    request.payload.paintingName = votingWinner;
-                }
-                break;
-            default:
-                break;
-        }
-        requestQueue.push(request);
-
-        if (request.chatState) chatBot.setState(request.chatState);
-        if (request.chatState && request.chatState === "voting"){
-            handleVoting();
-        }
-
-        setTimeout(() => {
-            debug("end session: " + request.name);
-            callback();
-        }, request.timeout * 1000);
-    }
-    else{
-        callback();
-    }
-}, 1);
-sessionQueue.drain = () => {
-    if (connectedClient){
-        debug("drained, reset!");
-        sessionQueue.push(sessions);
-    }
-    else{
-        debug("drainged but no client, so do nothing");
-    }
-};
-
-var handleVoting = function(){
-    console.log("handleVoting");
-    setTimeout(function(){
-        var list;
-        if (chatBot.votes){
-            list = chatBot.getVotingList();
-        }
-        if (connectedClient && chatBot.state === "voting"){
-            requestQueue.push({ name: "hudList", payload: list ? list : [] });
-            handleVoting();
-        }
-    }, 500);
-};
-
-let sessions = [
-    { name: "startTheme",           chatState: "voting",      payload: { sessionLength: 15 }, timeout: 15 },
-    { name: "endTheme",             chatState: null,          payload: { themeName: "dog" }, timeout: 4 },
-    { name: "startPainting",        chatState: "painting",    payload: { width: 100, height: 100, sessionLength: 60 }, timeout: 60 },
-    { name: "endPainting",          chatState: null,          payload: {}, timeout: 4 },
-    { name: "startNaming",          chatState: "voting",      payload: { sessionLength: 15 }, timeout: 15 },
-    { name: "endNaming",            chatState: null,          payload: { paintingName: "Twitch Masterpiece" }, timeout: 4 },
-];
-
 const randomColor = () => {
-    if (connectedClient){
-        connectedClient.emit("paint", {
-            x: Math.floor(Math.random() * 100) + 1,
-            y: Math.floor(Math.random() * 100) + 1,
-            rgb: {
-                r: Math.floor(Math.random() * 255) + 1,
-                g: Math.floor(Math.random() * 255) + 1,
-                b: Math.floor(Math.random() * 255) + 1
-            },
-            size: 1
-        });
-        setTimeout(function(){
-            randomColor();
-        }, 1);
-    }
+  if (connectedClient){
+    connectedClient.emit("paint", {
+      x: Math.floor(Math.random() * 100) + 1,
+      y: Math.floor(Math.random() * 100) + 1,
+      rgb: {
+        r: Math.floor(Math.random() * 255) + 1,
+        g: Math.floor(Math.random() * 255) + 1,
+        b: Math.floor(Math.random() * 255) + 1
+      },
+      size: 1
+    });
+    setTimeout(function(){
+      randomColor();
+    }, 1);
+  }
 };
 
 io.sockets.on("connection", (socket) => {
-    debug_socket('socket is connected!');
+  debug_socket('socket is connected!');
 
-    socket.on("start", (options) => {
-        debug_socket('socket sent start!');
-        connectedClient = socket;
-        if (options){
-            debug_socket(options);
-            debug_socket(typeof options);
-            async.each(sessions, (session, callback) => {
-                debug_socket(session);
-                async.each(options, (option, optionCallback) => {
-                    debug_socket(option);
-                    if (option.name === session.name) {
-                        debug("matched, set new sessionLength");
-                        session.timeout = option.sessionLength;
-                        session.payload.sessionLength = option.sessionLength;
-                    }
-                    optionCallback();
-                }, callback);
-            }, () => {
-                debug_socket("set all, start");
-                debug_socket(sessions);
-                sessionQueue.push(sessions);
-                chatBot.newStream("sebastaindeveloperaccount");
-                chatBot.addListener(requestQueue);
-            });
-        }
-        else{
-            debug_socket("got no options, start with default");
-            sessionQueue.push(sessions);
-            chatBot.newStream("sebastaindeveloperaccount");
-            chatBot.addListener(requestQueue);
-        }
-    });
+  socket.on("start", (options) => {
+    debug_socket('socket sent start!');
+    connectedClient = socket;
+    chatBot.newStream("twitchplace");
+    chatBot.addListener(requestQueue);
+  });
 
-    socket.on("error", (e) => {
-       debug(e);
-    });
+  socket.on("error", error => debug(error));
 
-    socket.on("disconnect", () => {
-        debug("CLIENT DISCONNECT");
-        if (socket === connectedClient) {
-            connectedClient = null;
-            chatBot.endStream();
-        }
-    });
-
+  socket.on("disconnect", () => {
+    debug("CLIENT DISCONNECT");
+    if (socket === connectedClient) {
+      connectedClient = null;
+      chatBot.endStream();
+    }
+  });
 });
 
 app.use(bodyParser());
 
 const storage = multer.diskStorage({
-    destination(req, file, cb){
-        cb(null, __dirname + "/upload/")
-    },
-    filename(req, file, cb){
-        cb(null, file.originalname);
-    }
+  destination(req, file, cb){
+    cb(null, __dirname + "/upload/")
+  },
+  filename(req, file, cb){
+    cb(null, file.originalname);
+  }
 });
 const upload = multer({ dest: __dirname + "/upload", storage: storage }).single("file");
 
 const uploadHandler = (req, res, next) => {
-    if (req.url !== '/upload' || req.method !== 'POST') return next()
-    upload(req, res, (err) => {
-        if (err) return next(err);
-    })
+  if (req.url !== '/upload' || req.method !== 'POST') return next()
+  upload(req, res, (err) => {
+    if (err) return next(err);
+  })
 }
 
 app.use(uploadHandler);
 app.post("/upload", (req, res) => {
-    // console.log(req);
-    res.send({ code: 200 });
+  // console.log(req);
+  res.send({ code: 200 });
 });
 app.listen(3000);
--- server/chatbot.js
+++ server/chatbot.js
@@ -1,5 +1,3 @@
-import async from "async";
-import moment from "moment";
 import irc from "tmi.js";
 import _ from "underscore";
 import color from "color";
@@ -8,8 +6,8 @@ import debug_module from "debug";
 const debug = debug_module("chat_bot");
 
 process.on('uncaughtException', function (err) {
-    console.error(err);
-    console.log("Node NOT Exiting...");
+  console.error(err);
+  console.log("Node NOT Exiting...");
 });
 
 var chatBot = {};
@@ -17,130 +15,76 @@ chatBot.players = {};
 chatBot.votes = {};
 
 chatBot.connect = function(channel){
-    if (chatBot.client){
-        chatBot.client = null;
-    }
-    chatBot.client = new irc.client({
-        options: {
-            debug: false
-        },
-        connection: {
-            random: "chat",
-            reconnect: true
-        },
-        identity: {
-            username: "octonombot",
-            password: "oauth:qwfselxf5nxlb1eimaeys4u4axuc35"
-        },
-        channels: ["#" + channel]
-    });
+  if (chatBot.client){
+    chatBot.client = null;
+  }
+  chatBot.client = new irc.client({
+    options: {
+      debug: false
+    },
+    connection: {
+      random: "chat",
+      reconnect: true
+    },
+    identity: {
+      username: "octonombot",
+      password: "oauth:qwfselxf5nxlb1eimaeys4u4axuc35"
+    },
+    channels: ["#" + channel]
+  });
 
-    chatBot.client.on("error", () =>{
-    });
+  chatBot.client.on("error", () =>{
+  });
 
-    chatBot.client.addListener("chat", (channel, user, message) => {
-        debug(user.username + ": " + message);
-        switch (chatBot.state) {
-            case "painting":
-                chatBot.handleChat(message, user.username);
-                break;
-            case "voting":
-                chatBot.handleVoting(message, user.username);
-                break;
-            default:
-                break;
-        };
-    });
+  chatBot.client.addListener("chat", (channel, user, message) => {
+    debug(user.username + ": " + message);
+    chatBot.handleChat(message, user.username);
+  });
 
-    chatBot.client.addListener("part", (channel, username) => {
-        debug("parted: " + username);
-    });
+  chatBot.client.addListener("part", (channel, username) => {
+    debug("parted: " + username);
+  });
 
-    chatBot.client.addListener("error", (err) => {
-        debug(err);
-    });
+  chatBot.client.addListener("error", (err) => {
+    debug(err);
+  });
 
-    chatBot.client.connect();
+  chatBot.client.connect();
 };
 
 chatBot.handleChat = (message, user) => {
-    console.log(chatBot.players);
-    if (message.indexOf("paint") === 0) {
-        var args = message.slice(6, message.length).split(" ");
-        var alpha = 1;
-        var size = 1;
-        args = _.map(args, (s, i) => {
-            if (i === 0 || i === 1){
-                return parseInt(s);
-            } else if (i === 2 && s.indexOf(",") != -1){
-                return "rgb(" + s + ")";
-            }
-            return s;
-        });
-        var rgb;
-        console.log("parse color");
-        try {
-            rgb = color(args[2]).rgb();
-        } catch (e) {
-            e = new Error(e);
-            throw e;
-        }
-        var req = {
-            name: "paint",
-            payload: {
-                x: args[0],
-                y: args[1],
-                rgb: rgb,
-                size: args.length > 4 ? parseInt(args[4]) : 1,
-            }
-        };
-        req.payload.rgb["a"] = args.length > 3 ? parseFloat(args[3]) : alpha;
-        chatBot.players[user] = req.payload;
-        if (chatBot.send) chatBot.send(req);
+  if (message.indexOf("paint") === 0) {
+    var args = message.slice(6, message.length).split(" ");
+    var alpha = 1;
+    var size = 1;
+    args = _.map(args, (s, i) => {
+      if (i === 0 || i === 1){
+        return parseInt(s);
+      } else if (i === 2 && s.indexOf(",") != -1){
+        return "rgb(" + s + ")";
+      }
+      return s;
+    });
+    var rgb;
+    try {
+      rgb = color(args[2]).rgb();
+    } catch (e) {
+      e = new Error(e);
+      throw e;
     }
-    else if (message.indexOf("alpha") === 0 && chatBot.players[user]){
-        var args = message.slice(6, message.length).split(" ");
-        if (args){
-            chatBot.players[user].rgb.a = parseInt(args[0]);
-        }
-    }
-    else if (message.indexOf("size") === 0 && chatBot.players[user]){
-        var args = message.slice(5, message.length).split(" ");
-        if (args){
-            chatBot.players[user].size = parseInt(args[0]);
-        }
-    }
-    else if (chatBot.players[user]){
-        let matched = false;
-        if (message === "r" || message === "re" || message === "redo") {
-            message = chatBot.players[user].lastCommand;
-        }
-        switch (message) {
-            case "left":
-                chatBot.players[user].x += chatBot.players[user].size;
-                chatBot.players[user].lastCommand = message;
-                matched = true;
-                break;
-            case "right":
-                chatBot.players[user].x -= chatBot.players[user].size;
-                chatBot.players[user].lastCommand = message;
-                matched = true;
-                break;
-            case "up":
-                chatBot.players[user].y += chatBot.players[user].size;
-                chatBot.players[user].lastCommand = message;
-                matched = true;
-                break;
-            case "down":
-                chatBot.players[user].y -= chatBot.players[user].size;
-                chatBot.players[user].lastCommand = message;
-                matched = true;
-                break;
-            default:
-                break;
-        }
-        if (chatBot.send && matched) chatBot.send({ name: "paint", payload: chatBot.players[user] });
-    }
+    var req = {
+      name: "paint",
+      payload: {
+        x: args[0],
+        y: args[1],
+        rgb: rgb,
+        size: args.length > 4 ? parseInt(args[4]) : 1,
+      }
+    };
+    req.payload.rgb["a"] = args.length > 3 ? parseFloat(args[3]) : alpha;
+    chatBot.players[user] = req.payload;
+    if (chatBot.send) chatBot.send(req);
+  }
 };
 
 chatBot.handleVoting = (message, user) => {
@@ -181,30 +125,30 @@ chatBot.getVotingWinner = () => {
 };
 
 chatBot.setState = (state) => {
-    debug("SET CHATSTATE: " + state);
-    chatBot.state = state;
-    switch (state) {
-        case "painting":
-            chatBot.players = {};
-            break;
-        case "voting":
-            chatBot.votes = {};
-        default:
-            break;
-    }
+  debug("SET CHATSTATE: " + state);
+  chatBot.state = state;
+  switch (state) {
+    case "painting":
+      chatBot.players = {};
+      break;
+    case "voting":
+      chatBot.votes = {};
+    default:
+      break;
+  }
 };
 
 chatBot.mode = "idle";
 chatBot.addListener = (requestQueue) => {
-    chatBot.send = requestQueue.push;
+  chatBot.send = requestQueue.push;
 };
 chatBot.newStream = (channel) => chatBot.connect(channel);
 
 chatBot.endStream = () => {
-    if (chatBot.client){
-        chatBot.client.disconnect();
-        chatBot.client = null;
-    }
+  if (chatBot.client){
+    chatBot.client.disconnect();
+    chatBot.client = null;
+  }
 };
 
 module.exports = chatBot;
